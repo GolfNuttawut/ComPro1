a94a8c9ff4acec51c8e3ca0c9ba053fe2a6ddd6b
GolfNuttawut <nuttawut.pinakaphang@gmail.com>
2024-12-05 19:21:30 +0700

    add if-else : Absolute, Grading, NumSort

---
+++ Week1/NumSort/NumSort.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+void main(){
+    int x, y, z;
+    scanf("%d %d %d",&x, &y, &z);
+    if(x<=y && x<=z){
+        if(y<=z){
+            printf("%d %d %d",x, y, z);
+        }
+        else{
+            printf("%d %d %d",x, z, y);
+        }
+    }
+    else if(y<=x && y<=z){
+        if(x<=z){
+            printf("%d %d %d",y, x, z);
+        }
+        else{
+            printf("%d %d %d",y, z, x);
+        }
+    }
+    else if(z<=x && z<=y){
+        if(x<=y){
+            printf("%d %d %d",z, x, y);
+        }
+        else{
+            printf("%d %d %d",z, y, x);
+        }
+    }
+}
