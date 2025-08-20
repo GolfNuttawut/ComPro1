cebb381b0c9cd06d9c47d7e484236f1e9f62b2f9
GolfNuttawut <nuttawut.pinakaphang@gmail.com>
2025-08-20 23:33:37 +0700

    Update OddVsEven

--- WhileLoop/OddVsEven/OddVsEven.c
+++ WhileLoop/OddVsEven/OddVsEven.c
@@ -1,23 +1,21 @@
 #include <stdio.h>
 void main(){
-    int i = 0, odd = 0, oddSum = 0, even = 0, evenSum = 0;
+    int i = 0, oddSum = 0, evenSum = 0;
     while(i < 8){
         int n;
         scanf("%d",&n);
         if(n%2==0){
             evenSum += n;
-            even++;
         }
         else{
             oddSum += n;
-            odd++;
         }
         i++;
     }
-    if(odd > even){
+    if(oddSum > evenSum){
         printf("odd\n%d\n%d",evenSum, oddSum);
     }
-    else if(odd < even){
+    else if(oddSum < evenSum){
         printf("even\n%d\n%d",evenSum, oddSum);
     }
     else{
