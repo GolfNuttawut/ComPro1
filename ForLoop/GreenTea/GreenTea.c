ecafe6cd4f9b39519b86d977f55185c47037dee7
GolfNuttawut <nuttawut.pinakaphang@gmail.com>
2025-08-21 00:03:03 +0700

    Update GreenTea

--- ForLoop/GreenTea/GreenTea.c
+++ ForLoop/GreenTea/GreenTea.c
@@ -1,27 +1,19 @@
 #include <stdio.h>
 void main(){
-    int water, sugar;
+    int water, sugar, waterUse, sugarUse, bottle;
     for(int i=0;i<7;i++){
         scanf("%d %d",&water, &sugar);
-        if(water/250 <= sugar/15){
-            printf("%d ",water/250);
-            water %= 250;
-            if(water == 0){
-                printf("\n");
-            }
-            else{
-                printf("water\n");
-            }
+        waterUse = water/250, sugarUse = sugar/15;
+        if(waterUse < sugarUse){
+            bottle = waterUse;
         }
         else{
-            printf("%d ",sugar/15);
-            water -= 250*(sugar/15);
-            if(water == 0){
-                printf("\n");
-            }
-            else{
-                printf("water\n");
-            }
+            bottle = sugarUse;
         }
+        printf("%d", bottle);
+        if(water-bottle*250 != 0){
+            printf(" water");
+        }
+        printf("\n");
     }
 }
