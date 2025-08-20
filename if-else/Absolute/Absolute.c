94634a2c662101f8ca7c324225bce439736a1c85
GolfNuttawut <nuttawut.pinakaphang@gmail.com>
2025-08-20 22:53:58 +0700

    Hot Fix 1
    update : TheVoiceBasic, Absolute

--- if-else/Absolute/Absolute.c
+++ if-else/Absolute/Absolute.c
@@ -1,11 +1,28 @@
 #include <stdio.h>
+
 void main(){
-    int x;
-    scanf("%d",&x);
-    if(x<0){
-        printf("%d",x*-1);
+    int borrow,year;
+    double total;
+    printf("กรอกจำนวนเงินที่ต้องการกู้: ");
+    scanf("%d",&borrow);
+    printf("กรอกจำนวนปีที่ต้องการกู้: ");
+    scanf("%d",&year);
+    if(borrow<15000){
+        total = borrow+borrow*0.02*year;
+        printf("คิดดอกเบี้ย 2%% ต่อปี\n");
+        printf("คุณต้องชำระคืน = %.2lf บาท", total);
+    }
+    else if(borrow<=120000){
+        total = borrow+borrow*0.018*year;
+        printf("คิดดอกเบี้ย 1.8%% ต่อปี\n");
+        printf("คุณต้องชำระคืน = %.2lf บาท", total);
     }
     else{
-        printf("%d",x);
+        int dif = borrow-120000;
+        total = borrow+(120000*0.017*year)+(dif*0.016*year);
+        printf("คิดดอกเบี้ยแบบขั้นบันได:\n");
+        printf("120000 แรก คิด 1.7%% ต่อปี\n");
+        printf("ส่วนเกิน %d คิด 1.6%% ต่อปี\n",dif);
+        printf("คุณต้องชำระคืน = %.2lf บาท", total);
     }
 }
--- if-else/TheVoiceBasic/TheVoiceBasic.c
+++ if-else/TheVoiceBasic/TheVoiceBasic.c
@@ -4,10 +4,20 @@ void main(){
     scanf("%d %d %d %d %d %d",&K, &K1, &K2, &S1, &S2, &G);
     if(S1 > 8 && S2 > 8){
         if(G==1){
-            printf("1");
+            if(K1<K){
+                printf("1");
+            }
+            else{
+                printf("2");
+            }
         }
         else if(G==2){
-            printf("2");
+            if(K2<K){
+                printf("2");
+            }
+            else{
+                printf("1");
+            }
         }
     }
     else if(S1 > 8){
